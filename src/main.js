9d1084b246bd12f505bf4d9ca45a49ab6aa90e40
LUHMLLO <luhmllo06@protonmail.com>
2024-02-10 12:13:15 -0400

    kinda working, router needs fixes

--- src/main.js
+++ src/main.js
@@ -1,20 +1,6 @@
 import './lib/css/main.css';
+import { Router } from './router';
 
 /** @type {Element | null} app */
 const app = document.querySelector('#app');
-
-if (app instanceof Element) {
-    switch (window.location.pathname) {
-        case '/':
-            app.innerHTML = './views/index.html';
-            break;
-
-        case '/components':
-            app.innerHTML = './views/components/index.html';
-            break;
-
-        default:
-            app.innerHTML = `404`;
-            break;
-    }
-}
+Router(app)
