d16c3e5985777a442f1ee88e1ec53530e753b9e1
LUHMLLO <luhmllo06@protonmail.com>
2024-02-17 11:04:51 -0400

    pager now runs as vite plugin

--- src/main.js
+++ src/main.js
@@ -3,12 +3,23 @@ import '../static/assets/css/main.reset.module.css';
 import '../static/assets/css/main.components.module.css';
 import '../static/assets/css/utils.module.css';
 import '../static/assets/css/widgets.module.css';
-import Pages from '~server/pages.js';
-import { App } from './classes/app';
+import { App } from './modules/app';
 
 const AppElement = document.getElementById('app');
 
-if (Pages && AppElement) {
-	const app = new App(AppElement, Pages);
-	app.mount();
+async function init() {
+	let Pages;
+	try {
+		Pages = await import('../.server/pages.js');
+	} catch (err) {
+		console.error('Failed to load pages:', err);
+		// Handle the error appropriately in your application
+	}
+
+	if (Pages && AppElement) {
+		const app = new App(AppElement, Pages.default);
+		app.mount();
+	}
 }
+
+init();
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,36 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import { Pager } from './lib/pager/index.mjs';
 
-export default defineConfig({
-	build: {
-		minify: 'terser',
-		outDir: 'dist',
-		target: ['ESNext'],
-		terserOptions: {
-			safari10: true,
-			ie8: true,
-			compress: true,
-			mangle: true,
-			module: true,
-			output: {
-				beautify: false,
+export default defineConfig(({}) => {
+	return {
+		build: {
+			minify: 'terser',
+			outDir: 'dist',
+			target: ['ESNext'],
+			terserOptions: {
+				safari10: true,
+				ie8: true,
+				compress: true,
+				mangle: true,
+				module: true,
+				output: {
+					beautify: false,
+				},
 			},
 		},
-	},
-	esbuild: {
-		drop: ['console', 'debugger'],
-	},
-	publicDir: './static',
-	resolve: {
-		alias: {
-			'~server': path.resolve(__dirname, './.server'),
-			'~lib': path.resolve(__dirname, './lib'),
-			'~src': path.resolve(__dirname, './src'),
-			'~root': path.resolve(__dirname, './'),
+		esbuild: {
+			drop: ['console', 'debugger'],
 		},
-	},
+		plugins: [Pager],
+		publicDir: './static',
+		resolve: {
+			alias: {
+				'~server': path.resolve(__dirname, './.server'),
+				'~lib': path.resolve(__dirname, './lib'),
+				'~src': path.resolve(__dirname, './src'),
+				'~root': path.resolve(__dirname, './'),
+			},
+		},
+	};
 });
