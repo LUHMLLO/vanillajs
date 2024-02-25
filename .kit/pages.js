0b23792fa0241cb0e4221902c01022676ab6f496
LUHMLLO <luhmllo06@protonmail.com>
2024-02-25 11:50:57 -0400

    need to work on login ages

--- .kit/pages.js
+++ .kit/pages.js
@@ -1,5 +1,6 @@
 import * as _src_pages__error_js from '~src/pages/+error.js';
 import * as _src_pages__page_js from '~src/pages/+page.js';
+import * as _src_pages_auth__page_js from '~src/pages/auth/+page.js';
 import * as _src_pages_preview__page_js from '~src/pages/preview/+page.js';
 import * as _src_pages_proposal__about_js from '~src/pages/proposal/+about.js';
 import * as _src_pages_proposal__page_js from '~src/pages/proposal/+page.js';
@@ -12,6 +13,10 @@ export default [
 	{
 		path: '/',
 		import: _src_pages__page_js,
+	},
+	{
+		path: '/auth',
+		import: _src_pages_auth__page_js,
 	},
 	{
 		path: '/preview',
---
+++ src/pages/auth/+page.js
@@ -0,0 +1,23 @@
+import Page from '~lib/packages/pager/@';
+
+export const page = new Page();
+
+page.SEO({
+	icon: '',
+	author: 'lm',
+	description: 'Vanilla JS - Auth Page',
+	image: '',
+	keywords: [''],
+	title: 'Auth',
+});
+
+let js = String.raw;
+page.JS(js``);
+
+let html = String.raw;
+page.HTML(html`
+    
+`);
+
+let css = String.raw;
+page.CSS(css``);
